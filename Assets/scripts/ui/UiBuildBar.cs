a6f103d9b9b80df6160afa9f013dc438f6034304
damnnn <damianojongerius@gmail.com>
2022-06-20 17:15:17 +0200

    build bar

---
+++ Assets/scripts/ui/UiBuildBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiBuildBar : MonoBehaviour
+{
+    public GameObject[] buttons;
+
+    // Start is called before the first frame update
+    [System.Obsolete]
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
