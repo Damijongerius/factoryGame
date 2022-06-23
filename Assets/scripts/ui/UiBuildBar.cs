722ab87276cad7dbbfc67ae8f3e883614eff1cb9
damij <damianojongerius@gmail.com>
2022-06-23 13:33:18 +0200

    full working ui

--- Assets/scripts/ui/UiBuildBar.cs
+++ Assets/scripts/ui/UiBuildBar.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class UiBuildBar : MonoBehaviour
 {
     public GameObject[] buttons;
+    public float width;
+    public float begin;
 
     // Start is called before the first frame update
-    [System.Obsolete]
     void Start()
     {
-        
+        InvokeRepeating("run", 0f, 5f);  
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void run()
+    {
+        int length = buttons.Length;
+        if(length % 2 == 0)
+        {
+            begin = (length * width) / -2;
+        }
+        else
+        {
+            begin = ((length - 1) / -2) * width;
+        }
+
+        for(int i = 0; i < length; i++)
+        {
+            buttons[i].transform.position = new Vector3(begin + (width * i) + (Screen.width / 2),transform.position.y,transform.position.z);
+            Debug.Log(begin + (width * i));
+        }
     }
 }
