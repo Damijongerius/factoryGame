384e40293ad177ce9ab8c2325deaed2f2399f6be
damij <damianojongerius@gmail.com>
2022-06-27 10:43:11 +0200

    new save sys not working

---
+++ Assets/scripts/save/PlayerProfile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProfile
+{
+    public string name;
+    public string currency;
+    public int xp;
+
+}
--- Assets/scripts/save/iSaveData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface iSaveData
-{
-    object CaptureState();
-    void RestoreState(object state);
-}
