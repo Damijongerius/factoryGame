5050b903bc5aa3b22f86be7c9b0a6352684c6683
Thimo <Thimo@it4me.nl>
2022-09-28 08:03:17 +0100

    changes
    ye

--- node/dist/index.js
+++ node/dist/index.js
@@ -1,12 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const { DB } = require("./DB/Database");
-//node module express
+const { saveFile } = require("./models/SaveFile");
 const { response, request, json } = require("express");
 const express = require("express");
 const app = express();
 app.use(express.json());
 let data;
 let database;
-//node module body parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,7 +20,11 @@ app.listen(3000, function () {
 app.post("/senddata", function (req, res) {
     console.log(req.body);
     let a = JSON.parse(req.body.sendJson);
+    console.log(saveFile.profile.Statistics.money);
+    saveFile.profile = new Profile();
+    saveFile.profile.Statistics = new Statistics();
+    //saveFile.Profile.Statistics.money = a.profile.Statistics.money;
     //data = JSON.parse(req.body);
-    console.log(a.map.grid);
+    // console.log(a.map.grid[0]);
 });
 //# sourceMappingURL=index.js.map
